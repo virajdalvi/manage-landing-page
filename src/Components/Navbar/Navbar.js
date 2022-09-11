94949b2bf0dfa8e43ef49b4de3b3f136a497c419
Viraj Dalvi <59591172+virajdalvi@users.noreply.github.com>
2022-09-11 13:19:26 +0530

    fixed nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,11 +6,9 @@ const toggleClass = () => {
   const btn = document.getElementById("menu-btn");
   const nav = document.getElementById("menu");
 
-  btn.addEventListener("click", () => {
-    btn.classList.toggle("open");
-    nav.classList.toggle("flex");
-    nav.classList.toggle("hidden");
-  });
+  btn.classList.toggle("open");
+  nav.classList.toggle("flex");
+  nav.classList.toggle("hidden");
 };
 
 const Navbar = () => {
